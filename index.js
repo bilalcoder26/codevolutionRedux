0c218ca71e23d98c444846f29c958e144f9f1068
Bilal Ahmed <ahmedalibilal26@gmail.com>
2021-04-06 17:49:15 +0530

    craeting a store

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
 const BUY_CAKE = "BUY_CAKE"; // string constant
 
 //Action Creator
@@ -23,9 +26,22 @@ const reducer = (state = initialState, action) => {
     case BUY_CAKE:
       return {
         ...state, //create the copy of state if there is another properties it will remain the same
-        numOfCakes: numOfCakes - 1,
+        numOfCakes: state.numOfCakes - 1,
       };
     default:
       return state;
   }
 };
+
+const store = createStore(reducer);
+console.log("initial State ", store.getState());
+const unsubscribe = store.subscribe(() =>
+  console.log("updated state", store.getState())
+);
+
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+
+unsubscribe();
